3610314370c4467f346f129c7f7ba1d18acafb1d
rebeicagrigoras <rebeicagrigoras25@gmail.com>
2025-01-03 15:26:20 +0000

    Created idInvalidCardCompanies function to categorise invalid cards by company

--- main.js
+++ main.js
@@ -62,3 +62,37 @@ function findInvalidCards(nestedArray) {
   }
   return invalidCards;
 }
+
+function idInvalidCardCompanies(invalidCards) {
+  const companies = [];
+  for (const card of invalidCards) {
+    const firstDigit = card[0];
+    switch (firstDigit) {
+      case 3:
+        if (!companies.includes("Amex")) {
+          companies.push("Amex");
+        }
+        break;
+      case 4:
+        if (!companies.includes("Visa")) {
+          companies.push("Visa");
+        }
+        break;
+      case 5:
+        if (!companies.includes("Mastercard")) {
+          companies.push("Mastercard");
+        }
+        break;
+      case 6:
+        if (!companies.includes("Discover")) {
+          companies.push("Discover");
+        }
+        break;
+      default:
+        if (!companies.includes("Company Not Found")) {
+          companies.push("Company Not Found");
+        }
+    }
+  }
+  return companies;
+}
